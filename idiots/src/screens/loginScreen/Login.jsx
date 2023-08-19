a93a6a269307d5241247ecf4bd3b7ed744955caa
SungChul Hong <hsc6263@gmail.com>
2023-08-19 22:21:48 +0900

    Add: login func

--- idiots/src/screens/loginScreen/Login.jsx
+++ idiots/src/screens/loginScreen/Login.jsx
@@ -1,19 +1,38 @@
 import {useNavigation} from '@react-navigation/native';
-import {View, TextInput, StyleSheet, Button} from 'react-native';
+import {View, TextInput, StyleSheet, Button, Alert} from 'react-native';
 import {height, width} from '../../../configs/globalStyles';
+import { useState } from 'react';
 
 export const Login = () => {
   const navigation = useNavigation();
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const validate = () => {
+    if (id !== 'admin' || pw !== 'admin1234') {
+      Alert.alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+    } else {
+      navigation.navigate('BottomTab');
+    }
+  };
+    
   return (
     <View style={styles.container}>
       <View style={styles.loginWrapper}>
         <View style={styles.logo} />
-        <TextInput style={styles.inputBox} />
-        <TextInput style={styles.inputBox} />
+        <TextInput 
+          style={styles.inputBox} 
+          onChangeText={text => setId(text)}
+          autoCapitalize='none'
+        />
+        <TextInput 
+          style={styles.inputBox} 
+          onChangeText={text => setPw(text)}
+          autoCapitalize='none'
+        />
         <Button
           title="input button"
           onPress={() => {
-            navigation.navigate('BottomTab');
+            validate();
           }}
         />
       </View>
